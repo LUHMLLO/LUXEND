71e3e1a760e328f4e3ed53f29cb6a644bc28a0d4
LUHMLLO <luis.jr.melo@gmail.com>
2021-02-11 01:56:58 -0400

    updating

--- src/views/app/Builder.vue
+++ src/views/app/Builder.vue
@@ -5,35 +5,34 @@
                 <canvas id="canvas" class="w-100 h-100"></canvas>
             </div>
         </div>
-        <div class="col-12 row debug justify-center content-center items-center" :class="[theme.primary_background,theme.primary_color]">
-
-            <section class="col-4 debug">
-                <h6 class="mb-0 font-sm mt-0">Opciones</h6>
-                <div class="row offset-x-16">
-                    <div class="col-1">
-                        <button class="border-none p-16 text-center content-center justify-center cursor-pointer" :class="[theme.primary_color,theme.secondary_background,theme.radius]">
-                            <span class="w-100 font-sm m-0">texto</span>
+        <div class="col-12 row" :class="[theme.primary_background,theme.primary_color]">
+            <section class="col-12">
+                <i class="uil uil-camera font-base mr-15 cursor-pointer" @pointerdown="screenshot"></i>
+                <i class="uil uil-save font-base cursor-pointer" @pointerdown="save"></i>
+            </section>
+            <section class="col-4 sm:col-12">
+                <h6 class="mb-5 font-sm mt-0">Opciones</h6>
+                <div class="row w-100">
+                    <div class="col-6 flex-grow-1 flex-shrink-0 p-1 d-inline-flex content-center justify-center overflow-hidden" v-for="(child,index) in computed_childs" :key="index">
+                        <button class="w-100 font-sm m-0 height-50 border border-white-fade-1 cursor-pointer" :class="[theme.primary_color,theme.secondary_background,theme.radius]">
+                            {{child.childID}}
                         </button>
                     </div>
                 </div>
             </section>
-            <section class="col-4 debug">
-                <h6 class="mb-0 font-sm mt-0">Texturas</h6>
-                <div class="row offset-x-16">
-                    <div class="col-1">
-                        <button class="border-none p-16 text-center content-center justify-center cursor-pointer" :class="[theme.primary_color,theme.secondary_background,theme.radius]">
-                            <span class="w-100 font-sm m-0">texto</span>
-                        </button>
+            <section class="col-4 sm:col-12">
+                <h6 class="mb-5 font-sm mt-0">Texturas</h6>
+                <div class="row w-100">
+                    <div class="col-3 flex-grow-1 p-1 d-inline-flex content-center justify-center overflow-hidden" v-for="(material,index) in material_textures" :key="index">
+                        <button class="border border-white-fade-1 cursor-pointer w-100 height-50 overflow-hidden" :class="[theme.radius]" :style="[{'background' : 'url('+material.texture+')'}]"/>
                     </div>
                 </div>
             </section>
-            <section class="col-4 debug">
-                <h6 class="mb-0 font-sm mt-0">Colores</h6>
-                <div class="row offset-x-16">
-                    <div class="col-1">
-                        <button class="border-none p-16 text-center content-center justify-center cursor-pointer" :class="[theme.primary_color,theme.secondary_background,theme.radius]">
-                            <span class="w-100 font-sm m-0">texto</span>
-                        </button>
+            <section class="col-4 sm:col-12">
+                <h6 class="mb-5 font-sm mt-0">Colores</h6>
+                <div class="row w-100">
+                    <div class="col-3 flex-grow-1 p-1 d-inline-flex content-center justify-center overflow-hidden" v-for="(material,index) in material_colors" :key="index">
+                        <button class="border border-white-fade-1 cursor-pointer w-100 height-50 overflow-hidden" :class="[theme.radius]" :style="[{'background' : '#'+material.color}]"/>
                     </div>
                 </div>
             </section>
@@ -47,6 +46,7 @@ import * as THREE from 'three';
 import { HalfFloatType } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter.js';
 import { BloomEffect, EffectComposer, EffectPass, RenderPass} from "postprocessing";
 export default {
     name: 'Visualizer',
@@ -59,15 +59,66 @@ export default {
             renderer: null,
             camera: null,
             controls: null,
+            hemisphereLight: null,
             dirLight: null,
             spotLight: null,
             composer: null,
             texture: null,
             material: null,
             model: null,
+            model_blob: null,
             model_children: [],
             //modelpath: "https://luxend.s3.us-east-2.amazonaws.com/chair_e60fbe28b1.glb"
-            modelpath: "/assets/models/luxend.glb"
+            modelpath: "/assets/models/luxend.glb",
+            materials: [
+                {
+                    texture: '/assets/textures/tim-mossholder-V6KKk5LCfI0-unsplash.jpg',
+                    size: [3,3,3],
+                    shininess: 0
+                },
+                {
+                    texture: '/assets/textures/diego-passadori-e6frrz-kh-0-unsplash.jpg',
+                    size: [3,3,3],
+                    shininess: 0
+                },
+                {
+                    texture: '/assets/textures/04.jpg',
+                    size: [3,3,3],
+                    shininess: 0
+                },
+                {
+                    texture: '/assets/textures/05.jpg',
+                    size: [3,3,3],
+                    shininess: 0
+                },
+                {
+                    texture: '/assets/textures/06.jpg',
+                    size: [4,4,4],
+                    shininess: 0
+                },
+                {color: 'F44336'},
+                {color: 'e91e63'},
+                {color: '9c27b0'},
+                {color: '673ab7'},
+                {color: '3f51b5'},
+                {color: '2196F3'},
+                {color: '03a9f4'},
+                {color: '00bcd4'},
+                {color: '009688'},
+                {color: '4CAF50'},
+                {color: '8bc34a'},
+                {color: 'cddc39'},
+                {color: 'ffeb3b'},
+                {color: 'ffc107'},
+                {color: 'ff9800'},
+                {color: 'ff5722'},
+                {color: '795548'},
+                {color: '607d8b'},
+                {color: '9e9e9e'},
+                {color: 'FFFFFF'},
+                {color: '000000'},
+            ],
+            imgData: null,
         }
     },
     created(){
@@ -81,7 +132,18 @@ export default {
         window.location.reload();
     },
     computed:{
-        computed_childs: function() {
+
+        material_textures() {
+            return this.materials.filter(function(material){
+                return material.texture
+            })
+        },
+        material_colors() {
+            return this.materials.filter(function(material){
+                return material.color
+            })
+        },
+        computed_childs() {
             return this.model_children
         },
         DevicePixelRatio_G: function(){
@@ -94,7 +156,6 @@ export default {
             if (window.devicePixelRatio !== undefined) {
                 ratio = window.devicePixelRatio;
             }
-            console.log(ratio)
             return ratio;
         }
     },
@@ -114,11 +175,12 @@ export default {
         setRender: function(){
             this.renderer = new THREE.WebGLRenderer( {
                  canvas: this.canvas,
-                 powerPreference: "high-performance",
+                 powerPreference: "default",
                  antialias: true,
-                 stencil: false,
-                 depth: false, 
-                 alpha: true
+                 stencil: true,
+                 depth: true, 
+                 alpha: true,
+                 preserveDrawingBuffer: true
             } );
 			
             
@@ -157,7 +219,8 @@ export default {
         },
 
         setLights: function(){
-            this.scene.add( new THREE.HemisphereLight( 0x448aff, 0x795548, 1.75 ) );
+            this.hemisphereLight = new THREE.HemisphereLight( 0x448aff, 0x795548, 1.75 )
+            this.scene.add( this.hemisphereLight );
 			this.dirLight = new THREE.DirectionalLight(0xf8f8f8, 4.50);
             this.dirLight.position.set(-50,50,50);
             this.dirLight.castShadow = true;
@@ -207,21 +270,29 @@ export default {
                 }});
 
                 self.model.traverse(child => {
-                    switch(child.name){
-                        case 'tv1':
-                            //self.setTextures(child)
+                    switch(child.name) {
+                        case 'Scene':
                             break;
-                        case 'tv2':                
-                            //console.log(child)
+                        case '':
                             break;
-                        case 'facultad':             
-                            //console.log(child)
+                        case null:
                             break;
-                        case 'carrera':             
-                            //console.log(child)
+                        case 'SketchUp':
                             break;
-                        default:             
+                        case 'skp_camera_Last_Saved_SketchUp_View':
                             break;
+                        case 'skp_camera_Scene_1':
+                            break;
+                        case 'skp_camera_Scene_2':
+                            break;
+                        default:
+                            self.model_children.push(
+                                {
+                                    childID: child.name , 
+                                    mtl: new THREE.MeshStandardMaterial( { color: 0xF8F8F8} ),
+                                    children: child.children,
+                                },
+                            );  
                     }
                 })
 
@@ -270,6 +341,7 @@ export default {
             this.camera.aspect = this.canvas_wrapper.clientWidth / this.canvas_wrapper.clientHeight;
             this.camera.updateProjectionMatrix();
             this.renderer.setSize(this.canvas_wrapper.clientWidth, this.canvas_wrapper.clientHeight);
+	        this.composer.setSize(this.canvas_wrapper.clientWidth, this.canvas_wrapper.clientHeight);
         },
 
 
@@ -298,7 +370,49 @@ export default {
                     this.model.position.x += step;
                     break;
             }
-        }
+        },
+        
+
+        screenshot: function() {
+            let self = this;
+            self.imgData = self.renderer.domElement.toDataURL();
+            let link = document.createElement('a');
+            link.style.display = 'none';
+            link.setAttribute('href', self.imgData);
+            link.setAttribute('download', "luxend model " + Date.now());
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        },            
+        save: function () {
+            let self = this;
+            const exporter = new GLTFExporter();
+            self.scene.remove( self.hemisphereLight);
+            self.scene.remove( self.dirLight);
+            self.scene.remove( self.spotLight);
+            exporter.parse( self.scene, function ( glb ) {
+                function makeblob(){
+                    self.model_blob = new Blob([glb], { type: "application/octet-stream" });
+                    self.model_blob.name = self.model.name
+                    //let file = {'model': blob, 'name': blob.name}
+                    //self.saveActualModelToDB(file)
+                    //console.log('save function started...saving to firestore');
+                }
+                makeblob()
+                function downloadtopc(){
+                    let d = document.createElement('a');
+                    d.href = window.URL.createObjectURL(self.model_blob);
+                    d.download = `${self.model_blob.name}.glb`;
+                    document.body.appendChild(d);
+                    d.click();
+                    document.body.removeChild(d);
+                }
+                downloadtopc()
+            }, {binary: true, embedImages: true} );
+            self.scene.add( self.hemisphereLight);
+            self.scene.add( self.dirLight);
+            self.scene.add( self.spotLight);
+        },
 
 
     },
